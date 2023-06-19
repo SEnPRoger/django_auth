6ebbf8ce9cf437704d01e8acd30a96c09201b2d1
SEnPRoger <dobchel3@gmail.com>
2023-06-19 15:52:46 +0300

    Changed in Login View from cookie to headers

--- account/views.py
+++ account/views.py
@@ -62,7 +62,7 @@ class AccountLogin(APIView):
                 response = Response({'status':'successfully logged',
                                     'access_token':access_token},
                                     status=status.HTTP_200_OK)
-                JWTToken.set_refresh_to_cookie(response, refresh_token, cookie_name='REFRESH-TOKEN')
+                JWTToken.set_refresh_to_header(response, refresh_token, header_name='REFRESH-TOKEN')
                 response['X-CSRFToken'] = csrf.get_token(request)
                 return response
             else:
