01f0d616fcdcfc0be069a7e0ba75c2535bfd25ba
SEnPRoger <dobchel3@gmail.com>
2023-06-27 01:09:42 +0300

    Added staticfiles_dirs to settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -145,7 +145,10 @@ USE_TZ = True # True by default
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = []
+#STATICFILES_DIRS = []
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
