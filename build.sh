86fa693515fbf2842f29b99103dd5c32027c3d7d
SEnPRoger <dobchel3@gmail.com>
2023-06-27 12:51:15 +0300

    Changed static_root

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -o errexit  # exit on error
 
 pip install -r requirements.txt
 
-mkdir -p /opt/render/project/src/static
+mkdir staticfiles
 
 python manage.py collectstatic --no-input
 python manage.py makemigrations
