acaf5eda262eec6dc8206e7b574497ae6903de44
SEnPRoger <dobchel3@gmail.com>
2023-07-07 17:40:04 +0300

    Added VerifiedAccount model

--- account/serializers.py
+++ account/serializers.py
@@ -78,7 +78,7 @@ class AccountVerifySerializer(serializers.ModelSerializer):
 
     def create_request(self):
         request = self.context['request']
-        return VerifiedAccount.objects.create(account=request.user)
+        return VerifiedAccount.objects.create(account=request.user, provided_by=None)
 
     def update_request(self, instance, validated_data):
         instance.is_verified = validated_data.get("is_verified")
