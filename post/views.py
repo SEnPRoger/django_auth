8243fe908f4b7d7a5374a84428e2c34439bad2a6
SEnPRoger <dobchel3@gmail.com>
2023-07-15 19:01:24 +0300

    Fixed file passing and added replies to response

--- post/views.py
+++ post/views.py
@@ -31,9 +31,56 @@ class PostViewSet(ModelViewSet):
     @action(methods=['get'], detail=True)
     @permission_classes([AllowAny])
     def get_posts_by_author(self, request, nickname=None, format=None):
-        posts = Post.objects.filter(author__nickname=nickname).order_by('-published_date')
+        posts = Post.objects.filter(author__nickname=nickname).order_by('-published_date').prefetch_related(
+            Prefetch('reply', queryset=Post.objects.select_related('author'))
+        )
         serializer = PostSerializer(posts, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+
+        post_obj = []
+        for post in serializer.data:
+
+            photos = []
+            for photo in post['photos']:
+                file_url = photo['file']
+            if file_url:
+                file_url = request.build_absolute_uri(file_url)
+            photos.append({'file': file_url})
+
+            reply = post['reply']
+            reply_data = None
+            if isinstance(reply, int):
+                # Handle the case where 'reply' is an integer (post ID)
+                reply_post = Post.objects.get(id=reply)
+                reply_serializer = PostSerializer(reply_post, context={'request': request})
+                reply_data = reply_serializer.data
+            elif reply:
+                # Handle the case where 'reply' is a nested object
+                reply_data = {
+                    'id': reply['id'],
+                    'content': reply['content'],
+                    'author_username': reply['author']['username'],
+                    'author_nickname': reply['author']['nickname'],
+                    'author_account_photo': reply['author']['account_photo'],
+                    'published_date': reply['published_date'],
+                    'is_edited': reply['is_edited'],
+                    'device': reply['device'],
+                }
+
+            post_data = {
+                'id': post['id'],
+                'content': post['content'],
+                'author_username': post['author_username'],
+                'author_nickname': post['author_nickname'],
+                'author_account_photo': post['author_account_photo'],
+                'published_date': post['published_date'],
+                'is_edited': post['is_edited'],
+                'device': post['device'],
+                'photos': photos,
+                'reply': reply_data,
+            }
+            post_obj.append(post_data)
+
+        return Response(post_obj, status=status.HTTP_200_OK)
         
     @action(methods=['get'], detail=True)
     @permission_classes([AllowAny])
